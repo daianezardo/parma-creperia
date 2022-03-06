6f0fae520e707d1798917b0692bc23f9008afed2
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2022-03-06 15:26:19 -0300

    implementa abrir/fechar carrinho

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,13 @@
-// const cartSidebarEl = document.querySelector('.cart-sidebar')
-// cartSidebarEl.classList.add('cart-sidebar-open')
+const cartSidebarEl = document.querySelector('.cart-sidebar')
+function openSidebar () {
+     cartSidebarEl.classList.add('cart-sidebar-open')
+}
+function closeSidebar () {
+     cartSidebarEl.classList.remove('cart-sidebar-open')
+}
+
+const btnCartEl = document.getElementById('btn-cart')
+btnCartEl.addEventListener('click', openSidebar)
+
+const btnCartcloseEl = document.getElementById('btn-close-cart')
+btnCartcloseEl.addEventListener('click', closeSidebar)
