b275df06dc1c20da932eaaf03673baf33376bea5
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2022-03-08 17:35:08 -0300

    exige produtos no site

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,27 +17,40 @@ const fetchProducts = () => {
     .then(res => res.json())
     .then(data => {
         const groupsRootEl = document.querySelector('#groups-root')
-        console.log('Acabou o Json', data.groups)
-        for (let contador = 0;contador < data.groups.length;contador++) {
-            console.log(data.groups[contador])
-        const groupSectionEl = getSelectionElement(data.groups[contador])
-        groupsRootEl.appendChild(groupSectionEl)
-        }
+     data.groups.forEach((item) => {
+         const groupSectionEl = getSelectionElement(item)
+         groupsRootEl.appendChild(groupSectionEl)  
+     })
     })
-    // .catch((error) => {
-
-    //     console.log('Deu ruim')
+    .catch(() => {
+        console.log('Deu ruim')
         
-    //     console.log(error)
-        
-    //     })
+         })
 }
     const getSelectionElement = (group) => {
         const sectionEl = document.createElement('section')
         const sectionTitleEl = document.createElement('h2')
         sectionTitleEl.textContent = group.name
         sectionEl.appendChild(sectionTitleEl)
+        const productsGridEl = document.createElement('div')
+        productsGridEl.classList.add('products-grid')
+        sectionEl.appendChild(productsGridEl)
+        group.products.forEach((product) => {
+            const cardWrapEl = document.createElement('article')
+            cardWrapEl.classList.add('card')
+            cardWrapEl.innerHTML = `
+            <img src="${product.image}" alt="${product.name}" width="316" height="193">
+            <div class="card-content">
+            <h3>${product.name}</h3>
+            <p class="price">R$ ${product.price.toLocaleString('pt-br', { minimumFractionDigits: 2})}</p>
+            ${product.description ? `<p>${product.description}<p>` : ' '}
+            <button class="btn btn-main">Adicionar</button>
+        </div> 
+        `
+
+            productsGridEl.appendChild(cardWrapEl)
+        })
+
         return sectionEl
     }
-
-fetchProducts() 
+fetchProducts()
