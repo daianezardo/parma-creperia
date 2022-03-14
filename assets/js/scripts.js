392d5c86e2d914874629ce6f014c46cbe0310ca0
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2022-03-14 20:26:25 -0300

    corta carrinho de produtos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,11 @@
 const cartSidebarEl = document.querySelector('.cart-sidebar')
-function openSidebar () {
-     cartSidebarEl.classList.add('cart-sidebar-open')
+function openSidebar() {
+    cartSidebarEl.classList.add('cart-sidebar-open')
 }
-function closeSidebar () {
-     cartSidebarEl.classList.remove('cart-sidebar-open')
+function closeSidebar() {
+    cartSidebarEl.classList.remove('cart-sidebar-open')
 }
+
 
 const btnCartEl = document.getElementById('btn-cart')
 btnCartEl.addEventListener('click', openSidebar)
@@ -15,34 +16,34 @@ btnCartcloseEl.addEventListener('click', closeSidebar)
 const fetchProducts = () => {
     const groupsRootEl = document.querySelector('#groups-root')
     fetch('/products.json')
-    .then(res => res.json())
-    .then(data => {
-     data.groups.forEach((item) => {
-         const groupSectionEl = getSelectionElement(item)
-         groupsRootEl.appendChild(groupSectionEl)  
-     })
-    })
-    .catch(() => {
-        groupsRootEl.innerHTML = '<p class="error-alert">Falha ao buscar produtos. Por favor tente novamente.</p>'
-        
-         })
+        .then(res => res.json())
+        .then(data => {
+            data.groups.forEach((item) => {
+                const groupSectionEl = getSelectionElement(item)
+                groupsRootEl.appendChild(groupSectionEl)
+            })
+        })
+        .catch(() => {
+            groupsRootEl.innerHTML = '<p class="error-alert">Falha ao buscar produtos. Por favor tente novamente.</p>'
+
+        })
 }
-    const getSelectionElement = (group) => {
-        const sectionEl = document.createElement('section')
-        const sectionTitleEl = document.createElement('h2')
-        sectionTitleEl.textContent = group.name
-        sectionEl.appendChild(sectionTitleEl)
-        const productsGridEl = document.createElement('div')
-        productsGridEl.classList.add('products-grid')
-        sectionEl.appendChild(productsGridEl)
-        group.products.forEach((product) => {
-            const cardWrapEl = document.createElement('article')
-            cardWrapEl.classList.add('card')
-            cardWrapEl.innerHTML = `
+const getSelectionElement = (group) => {
+    const sectionEl = document.createElement('section')
+    const sectionTitleEl = document.createElement('h2')
+    sectionTitleEl.textContent = group.name
+    sectionEl.appendChild(sectionTitleEl)
+    const productsGridEl = document.createElement('div')
+    productsGridEl.classList.add('products-grid')
+    sectionEl.appendChild(productsGridEl)
+    group.products.forEach((product) => {
+        const cardWrapEl = document.createElement('article')
+        cardWrapEl.classList.add('card')
+        cardWrapEl.innerHTML = `
             <img src="${product.image}" alt="${product.name}" width="316" height="193">
             <div class="card-content">
             <h3>${product.name}</h3>
-            <p class="price">R$ ${product.price.toLocaleString('pt-br', { minimumFractionDigits: 2})}</p>
+            <p class="price">R$ ${product.price.toLocaleString('pt-br', { minimumFractionDigits: 2 })}</p>
             ${product.description ? `<p>${product.description}</p>` : ' '}
             <button class="btn btn-main">Adicionar</button>
         </div> 
@@ -52,39 +53,41 @@ const fetchProducts = () => {
             addTocart(product)
         })
 
-            productsGridEl.appendChild(cardWrapEl)
-        })
+        productsGridEl.appendChild(cardWrapEl)
+    })
 
-        return sectionEl
-    }
+    return sectionEl
+}
 fetchProducts()
 
-    const productsCart = []
-    const addTocart = newProduct => {
-            const productIndex = productsCart.findIndex(
-                item => item.id === newProduct.id
-            )
+const productsCart = []
+const addTocart = newProduct => {
+    const productIndex = productsCart.findIndex
+    
+    (item => item.id === newProduct.id)
 
-        if (productIndex === -1) {
-            productsCart.push({
+    if (productIndex === -1) {
+        productsCart.push(
+            {
                 ...newProduct,
                 qty: 1
-            })
-        } else {
-            productsCart[productIndex].qty++
-        }
+            }
+        )
+    } else {
+        productsCart[productIndex].qty++
+    }
 
-        handleCartUpdade()
+    handleCartUpdade()
+}
+
+const handleCartUpdade = () => {
+    if (productsCart.length > 0) {
+        const CartBadgeEl = document.querySelector('.btn-cart-badge')
+        CartBadgeEl.classList.add('btn-cart-badge-show')
+        let total = 0
+        productsCart.forEach(product => {
+            total = total + product.qty
+        })
+        CartBadgeEl.textContent = total
     }
-        
-    const handleCartUpdade = () => {
-         if (productsCart.length > 0) {
-             const CartBadgeEl = document.querySelector('.btn-cart-badge')
-             CartBadgeEl.classList.add('btn-cart-badge-show')
-             let total = 0
-             productsCart.forEach(product => {
-                 total = total + product.qty
-             })
-             CartBadgeEl.textContent = total
-         }
-    }
+}
